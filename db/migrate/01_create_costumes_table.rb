a9d492f8cc02adbf324ff471747aae6968859525
Serah-Njenga <serah35.nn@gmail.com>
2023-06-02 11:31:23 +0300

    Completed assignment

---
+++ db/migrate/01_create_costumes_table.rb
@@ -0,0 +1,11 @@
+class 01CreateCostumesTable.rb < ActiveRecord::Migration[6.1]
+    def change
+      create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :image_url
+        t.string :size
+        t.timestamps
+      end
+    end
+  end
